c76f14968f56a8d09c962fcb8eded3b8432e2b7e
Jane Venter <janeanneventer@gmail.com>
2020-09-15 20:43:57 +0000

    Automatically backed up by Learn

--- lib/oo_smallest_multiple.rb
+++ lib/oo_smallest_multiple.rb
@@ -1,5 +1,21 @@
 # Implement your object-oriented solution here!
 class SmallestMultiple
   
-  def initialiaze
+  attr_accessor :count
+  
+  def initialize(count)
+    @count = count
+  end
+  
+  def check_if_evenly_divisible(number)
+    evenly = false 
+    (2..self.count).each do |i| 
+      if number % i == 0 
+        evenly = true 
+      else 
+        return false 
+      end
+    end
+    return evenly
+  end
 end
